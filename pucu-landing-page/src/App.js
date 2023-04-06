7bf00c625f48f48b055510628dbd605cacf7a493
Jimmark Daroy <111548265+jimmark1@users.noreply.github.com>
2023-04-06 19:12:54 +0800

    Update App.js

--- pucu-landing-page/src/App.js
+++ pucu-landing-page/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import CNavbar from "./Components/CNavbar";
+import MainContent from "./Components/MainContent";
+import Footer from "./Components/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     return (
+          <div className="App container-fluid d-flex flex-column ">
+               <header className="row " id="page-header">
+                    <div className="col-12">
+                         <CNavbar />
+                    </div>
+               </header>
+               <main className="row flex-grow-1 " id="page-content">
+                    <div className="main-content col-12">
+                         <MainContent />
+                    </div>
+               </main>
+               <footer className="row bg-light">
+                    <div className="col-12">
+                         <Footer />
+                    </div>
+               </footer>
+          </div>
+     );
 }
-
 export default App;
