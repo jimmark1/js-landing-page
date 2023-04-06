5a54d424fb51427d45772b9eff9a5fa62f665eb3
Jimmark Daroy <111548265+jimmark1@users.noreply.github.com>
2023-04-06 19:13:11 +0800

    Create UPCourses.js

---
+++ pucu-landing-page/src/Components/UPCourses.js
@@ -0,0 +1,45 @@
+import accountancy from "../Images/Courses/accountancy.jpg";
+import computerScience from "../Images/Courses/Cs.jpg";
+import criminology from "../Images/Courses/Crim.jpg";
+import abEnglish from "../Images/Courses/abel.jpg";
+import architecture from "../Images/Courses/Archi.jpg";
+import CivilEngineering from "../Images/Courses/CE.jpg";
+import entrepreneurship from "../Images/Courses/entrep.jpg";
+import tourism from "../Images/Courses/TM.jpg";
+
+export const data = [
+     {
+          title: "AB English Language",
+          image: abEnglish,
+     },
+     {
+          title: "BS Accountancy",
+          image: accountancy,
+     },
+
+     {
+          title: "BS Architecture",
+          image: architecture,
+     },
+     {
+          title: "BS Civil Engineering",
+          image: CivilEngineering,
+     },
+     {
+          title: "BS Computer Science",
+          image: computerScience,
+     },
+     {
+          title: "BS Criminology",
+          image: criminology,
+     },
+
+     {
+          title: "BS Entrepreneurship",
+          image: entrepreneurship,
+     },
+     {
+          title: "BS Tourism Management",
+          image: tourism,
+     },
+];
